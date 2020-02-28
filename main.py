550bf40938c41b5cf215fe056d90dbf9df1b6741
klzuni43 <60404123+klzuni43@users.noreply.github.com>
2020-02-28 13:22:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ def on_draw():
 pyglet.clock.schedule(update)
 pyglet.app.run()
 
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((800, 600))
+img = pygame.image.load('assets/forest-assets/door.png')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    win.fill((0, 0, 0))
+    win.blit(img, (400, 300))
+
+
+    pygame.display.update()
+
+pygame.quit()
